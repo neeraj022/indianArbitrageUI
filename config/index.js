fc6e249a7b2743a48e121e5569e7c6fe8d51a021
neeraj <neerajy@dewsolutions.in>
2018-02-08 18:42:20 +0530

    changing server ip and socket io configuration

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
 export default {
-  // SERVER_HOST: 'http://ec2-18-219-71-232.us-east-2.compute.amazonaws.com'
-  SERVER_HOST: 'http://localhost:7000'
+  SERVER_HOST: 'http://ec2-18-219-71-232.us-east-2.compute.amazonaws.com:7000'
+  //SERVER_HOST: 'http://localhost:7000'
 }
--- plugins/socketio.js
+++ plugins/socketio.js
@@ -1,7 +1,9 @@
+import config from '~/config'
 import Vue from 'vue';
 import VueSocketio from 'vue-socket.io';
 //import socketio from 'socket.io';
-Vue.use(VueSocketio, 'ws://localhost:7000');
+// Vue.use(VueSocketio, 'ws://localhost:7000');
+Vue.use(VueSocketio, config.SERVER_HOST);
 
 //export const SocketInstance = socketio('ws://echo.websocket.org');
 
