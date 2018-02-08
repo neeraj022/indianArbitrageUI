c89e0712af0df97b384b091a58e91e2fdadc74c7
neeraj <neerajy@dewsolutions.in>
2018-02-08 18:55:59 +0530

    favicon change

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'indianarbitrageui',
+    title: 'Indian Crypto Watch',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
